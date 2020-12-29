de3228dd40a80d499f9a27cb064083bb59d6fbbb
Lloyd <54277450+pypinkfloydfan@users.noreply.github.com>
2020-12-29 13:25:43 +0000

    added setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import pathlib
+
+
+# directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# README file text
+README = (HERE / 'README.md').read_text()
+
+setup(name='usefulfunctions',
+      version='1.0.0',
+      description='My collection of useful functions',
+      long_description=README,
+      author='Lloyd Greensite',
+      packages=find_packages(),
+      url='https://github.com/pynkfloydfan/UsefulFunctions'
+      )
