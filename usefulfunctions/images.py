976744221793e96aeafff434779153f467be2691
Lloyd <54277450+pypinkfloydfan@users.noreply.github.com>
2020-12-30 00:49:32 +0000

    Update cutimage function
    Add option to save images and specify a list of filenames for each image. Changed the return so it returns a list of lists of the cut images rather than a statement saying images are saved.

--- usefulfunctions/images.py
+++ usefulfunctions/images.py
@@ -3,12 +3,15 @@ import os
 
 
 # set the default location of images to the Pictures directory (WINDOWS)
-image_path = os.path.join(os.environ['USERPROFILE'], 'Pictures')
+_image_path = os.path.join(os.environ['USERPROFILE'], 'Pictures')
 
 
-def cutimage(image, x_slices, y_slices, outpath=image_path):
+def cutimage(image, columns, rows, 
+             save_images=True, 
+             save_path=_image_path,
+             file_names=None):
     """
-    Cuts an image into equal sized slices, x_slices long and y_slices high
+    Cuts an image into equal sized slices, columns long and rows high
 
     I wanted to expand an image over 2 pages in a pdf (via LaTeX) but 
     unfortunately it wouldn't automatically stretch over 2 pages, so instead I 
@@ -20,41 +23,63 @@ def cutimage(image, x_slices, y_slices, outpath=image_path):
     Parameters:
     -----------
     image: an image file
-    x_slices: number of equal sized slices in the horizontal axis
-    y_slices: number of equal sized slices in the vertical axis
+    columns: number of equal sized slices in the horizontal axis
+    rows: number of equal sized slices in the vertical axis
+    save_images: if True then save all images cut from the main image
+    save_path: the place to save all cut images if save_images=True
+               the default path is the "Pictures" folder
+    file_names: a list of names for each image to be saved.
+                file_names=None then the images are named "slice_"+row+"_"+column
     
     Returns:
     --------
-    x_slices number of lists each with y_slices number of image items
+    List of row number of sub-lists, each sub-list has column number of images
+    Optionally, each image cut out can be saved
     """
 
     # x, y image size
     xsize, ysize = image.size
     
     # size of each slice in each axis
-    xstep = xsize // x_slices
-    ystep = ysize // y_slices
+    xstep = xsize // columns
+    ystep = ysize // rows
+    
+    # how many images will be created?
+    # nbImages = rows * columns
     
     # all coordinates of each sub-image
-    if x_slices > 1:
-        xcoord = list(range(0, xsize + x_slices - 1, xstep))
+    if columns > 1:
+        xcoord = list(range(0, xsize + columns - 1, xstep))
     else:
         xcoord = [0, xsize]
     if xcoord[-1] < xsize: 
         xcoord[-1] = xsize
     
-    if y_slices > 1:
-        ycoord = list(range(0, ysize + y_slices - 1, ystep))
+    if rows > 1:
+        ycoord = list(range(0, ysize + rows - 1, ystep))
     else:
         ycoord = [0, ysize]
     if ycoord[-1] < ysize: 
         ycoord[-1] = ysize
     
-    for y in range(y_slices):
-        for x in range(x_slices):
+    result = list()
+    img = 0 # counter for images
+    
+    for y in range(rows):
+        x_list = list()
+        for x in range(columns):
             bbox = (xcoord[x], ycoord[y], xcoord[x+1], ycoord[y+1])
             image_slice = image.crop(bbox)
-            image_slice.save(os.path.join(outpath, "slice_" + str(x) + "_" + str(y) + "_" + ".png"))
+            x_list.append(image_slice)
+            
+            if save_images:
+                if file_names is None:
+                    image_name = "slice_" + str(x) + "_" + str(y) + ".png"
+                else:
+                    image_name = file_names[img]
+                    
+            image_slice.save(os.path.join(save_path, image_name))
             # image_slice.show()
+        result.append(x_list)
     
-    return 'All images saved'
+    return result
